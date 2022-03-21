5db824e6aa13a7df15a7247785d5c4d149799f88
father <102065818+father121@users.noreply.github.com>
2022-03-22 00:09:08 +0530

    Add files via upload

---
+++ javascipt.js
@@ -0,0 +1,32 @@
+function tati() {
+    let toe = "me";
+    document.getElementById('doso').innerHTML = toe;
+}
+function myold() {
+    let x,y,z;
+    x=12;
+    y=11;
+    z=x*y;
+    document.getElementById('five').innerHTML = z
+
+    
+}
+function intel() {
+    let x = 2/0;
+    document.getElementById('never').innerHTML = x;
+
+}
+function noe() {
+    let x  = ("health,wealth,nature");
+    document.getElementById('next').innerHTML = x.slice(1);
+
+}
+function warn() {
+    alert("warning: this can damage your smartphone")
+    
+}
+let con = 12;
+let sol = 11;
+console.log(con*sol);
+let aph = ["new","old","past","here"];
+console.log(aph);
